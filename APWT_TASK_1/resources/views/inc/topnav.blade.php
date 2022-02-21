8852bab91b7fca783c04ffa839ecce413e64ba02
Your Name <you@example.com>
2022-02-22 00:21:38 +0600

    _40

--- APWT_TASK_1/resources/views/inc/topnav.blade.php
+++
@@ -1,6 +0,0 @@
-<a class="btn btn-Info" href="{{route('home')}}">Home</a>
-<a class="btn btn-Info" href="{{route('contact')}}">Contact</a>
-<a class="btn btn-Info" href="{{route('about')}}">About</a>
-<a class="btn btn-Info" href="{{route('ourteams')}}">Our Teams</a>
-<a class="btn btn-Info" href="{{route('product')}}">Product</a>
-
